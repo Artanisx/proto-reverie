be66bf2f9308fba5506c898078096e93d3834b32
Artanis <fabrizio+git@fabriziotobia.it>
2026-08-05 12:37:54 +0200

    Enahcning the slash, emitting damage to the enemy after a certain point of the animation
    - shoudnt' this be a method called from the animation instead?

--- scenes/characters/player/states/player_state_slashing.gd
+++ scenes/characters/player/states/player_state_slashing.gd
@@ -8,18 +8,33 @@ extends PlayerState
 ## Transitions:
 ## Slashing > Moving
 
+const TIME_EMIT_DAMAGE: int = 200 # How many milliseconds after the player slash animation is started, the damage should be emitted. This is to avoid the enemy getting damage when the player is charging the slash.
+
+var has_emitted_damage: bool = false # To emit damage only once per slash
+var time_start_slash: int = Time.get_ticks_msec()	# Var to store the time since start slashing, used with the TIME_EMIT_DAMAGE constant to time the damage mission
+
+
 ## Execute what needs to be done immediately when the node enters the tree, so when we switch to this state (basically kind of a _ready)	
 func  _enter_tree() -> void:
 	## Play the slash animation
 	player.animation_player.play("slash")
 	
 	## Hookup to the finish signal
-	player.animation_player.animation_finished.connect(on_animation_finished)
+	player.animation_player.animation_finished.connect(on_animation_finished)	
+		
+func _process(_delta: float) -> void:
+	## Start the timer
+	var time_elasped := Time.get_ticks_msec() - time_start_slash
 	
-	## Check wheter an enemy is in reach (so it will get it)
-	if player.weapon_reach_raycast.is_colliding():
-		var enemy := player.weapon_reach_raycast.get_collider() as Enemy
-		enemy.try_receive_hit()	 ## try to hit the enemy that collided with the raycast
+	## Check if it's time to emit damage (only once) and only after the right moment (200 ms)
+	if not has_emitted_damage and time_elasped > TIME_EMIT_DAMAGE:
+		has_emitted_damage = true
+		
+		## Check wheter an enemy is in reach (so it will get it) - only emit damage if weapon collided
+		if player.weapon_reach_raycast.is_colliding():
+			var enemy := player.weapon_reach_raycast.get_collider() as Enemy
+			if enemy != null:
+				enemy.try_receive_hit()	 ## try to hit the enemy that collided with the raycast	
 		
 ## Since we want to be able to move while slashing, we call player.process() super
 func _physics_process(delta: float) -> void:
