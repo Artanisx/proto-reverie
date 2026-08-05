7c5b833410312d840d9f5751e2f316a09337b3eb
Artanis <fabrizio+git@fabriziotobia.it>
2026-08-05 16:59:47 +0200

    Fixing enemy facing the player issues and gravity issues

--- scenes/characters/enemies/states/enemy_state_moving.gd
+++ scenes/characters/enemies/states/enemy_state_moving.gd
@@ -18,8 +18,12 @@ func _enter_tree() -> void:
 	
 func _physics_process(delta: float) -> void:
 	if enemy.has_registered_player():
+		## FIrst we save the Y commponent of the enemy transform so we can be sure it is not altered during the rotation
+		var target_position := enemy.player.global_position		## we get the player position
+		target_position.y = enemy.global_position.y ## vertically we save the ENEMY (this) position to avoid altering it facing the player with te whole body
+		
 		## Face the player, so calculate the rotation that the enemy needs to perform to look at the player
-		var target_transform: Transform3D = enemy.global_transform.looking_at(enemy.player.global_position)
+		var target_transform: Transform3D = enemy.global_transform.looking_at(target_position)
 		
 		## slowly rotate towards that target transform (to face the player)
 		enemy.global_basis = enemy.global_basis.slerp(target_transform.basis, delta * SPEED_ROTATION)		
@@ -40,7 +44,12 @@ func _physics_process(delta: float) -> void:
 			enemy.animation_player.play("run")
 			
 			## Set the velocity to move forwad so z axis (since we already turned towards the player, means we're moving towards the player)
-			enemy.velocity = -enemy.global_basis.z * enemy.speed
+			## but... in order to apply movement forward and not touch the Y componetn (so gravity is affect) we need to exclude it from the movement
+			var target_velocity = -enemy.global_basis.z * enemy.speed
+			target_velocity.y = enemy.velocity.y ## Preserve the enemy original Y velocity so we don't mess with the gravity
+			
+			## Apply the velocity to apply movement	
+			enemy.velocity = target_velocity
 			
 			
 		## Make sure we process enemy movement or else any change to velocity will do nothing as move_and_slide() call is required to make them effective
