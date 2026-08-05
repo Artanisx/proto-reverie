a508de08d645d3ae4771e75227482796f9003742
Artanis <fabrizio+git@fabriziotobia.it>
2026-08-05 09:37:24 +0200

    Adding a Slashing and Hurt animation in blender, then implementing the SLASHING player state

---
+++ scenes/characters/player/states/player_state_slashing.gd
@@ -0,0 +1,20 @@
+class_name PlayerStateSlashing
+extends PlayerState
+
+## Player State: Slashing
+##
+## This handles the behaviour for the player state: State.SLASHING
+## It contains all processing, signals, etc required for this state
+## Transitions:
+## Slashing > Moving
+
+## Execute what needs to be done immediately when the node enters the tree, so when we switch to this state (basically kind of a _ready)	
+func  _enter_tree() -> void:
+	## Play the slash animation
+	player.animation_player.play("slash")
+	
+	## Hookup to the finish signal
+	player.animation_player.animation_finished.connect(on_animation_finished)
+	
+func on_animation_finished(_animation_name: String) -> void:	
+	transition_state(Player.State.MOVING)	## Emit the signal and transition to Moving
