95a6b5406a5e3d3839b2f08f60a1706b4929164c
Artanis <fabrizio+git@fabriziotobia.it>
2026-08-10 14:02:26 +0200

    Player blocking state implemented

--- scenes/characters/player/states/player_state_moving.gd
+++ scenes/characters/player/states/player_state_moving.gd
@@ -10,6 +10,7 @@ extends PlayerState
 ## Moving > Throwing
 ## Moving > Slashing
 ## Moving > Kicking
+## Moving > Blocking
 
 ## All input code needs to stay in _process
 func _process(_delta: float) -> void:
@@ -18,16 +19,20 @@ func _process(_delta: float) -> void:
 		transition_state(Player.State.PICKING_UP)	## Emit the signal with the state to transition to
 		
 	## Setup for the thrown button (R) to thrown an object and if he can thrown an object...
-	if Input.is_action_just_pressed("throw") and player.equipment.has_weapon():	
+	elif Input.is_action_just_pressed("throw") and player.equipment.has_weapon():	
 		transition_state(Player.State.THROWING)	## Emit the signal with the state to transition to		
 		
 	## Setup for the thrown button (LMB) to slash with the melee weapon that player should have equipped...
-	if Input.is_action_just_pressed("action") and player.equipment.has_weapon():	
+	elif Input.is_action_just_pressed("action") and player.equipment.has_weapon():	
 		transition_state(Player.State.SLASHING)	## Emit the signal with the state to transition to		
 		
 	## Setup for the thrown button (F) to kick
-	if Input.is_action_just_pressed("kick"):	
+	elif Input.is_action_just_pressed("kick"):	
 		transition_state(Player.State.KICKING)	## Emit the signal with the state to transition to		
+	
+	## Setup for the block button (RMB) to block
+	elif Input.is_action_just_pressed("block") and player.equipment.has_shield():	
+		transition_state(Player.State.BLOCKING)	## Emit the signal with the state to transition to	
 
 ## All movement/animation code needs to be processed at physic_process 
 func _physics_process(delta: float) -> void:
