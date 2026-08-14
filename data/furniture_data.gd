c182e35929a8eefe15ffd37cec41766309c2f2be
Artanis <fabrizio+git@fabriziotobia.it>
2026-08-14 16:28:57 +0200

    Adding furniture data and updating the pickable item to work with furniture as well
    Updaing the player to collide with the funrtire, but... not yet the enemy because I fear isseus wit hthe skeleton and I want to do it in a separate commit.

---
+++ data/furniture_data.gd
@@ -0,0 +1,11 @@
+class_name FurnitureData
+extends Resource
+
+## Furniture Data Resource.
+##
+## This contains just data for fornitures, like barrels, in the format of a Resource.
+## It has a regular mesh (when not broken) and a mesh for fragments.
+
+@export var name: String
+@export var glb_mesh: PackedScene
+@export var glb_fragemnts_mesh: PackedScene
