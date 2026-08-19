c4b1136730567c8290a8491c3faa0fa5541c5e67
Artanis <fabrizio+git@fabriziotobia.it>
2026-08-19 18:16:04 +0200

    Reworked the load code in world so that it loads testprocedurallevel programmatically

--- scenes/world/world.gd
+++ scenes/world/world.gd
@@ -3,45 +3,38 @@ extends Node3D
 
 @onready var minimap_camera: MinimapCamera = $MarginContainer/PanelContainer/Minimap/SubViewport/MinimapCamera
 
-@onready var test_procedural_level: BaseProceduralLevel = $TestProceduralLevel
+const LEVEL := preload("res://scenes/levels/test_procedural_level.tscn")
 
 ## LIST OF LEVELS - ONLY USED BY TUTORIAL 
 ##const LEVELS := [preload("res://scenes/levels/level_01_welcome.tscn")] ## Only used by tutorial
 
-var current_level_index := 0  ## Only used by tutorial
-var current_loaded_level : BaseLevel = null  ## Only used by tutorial
-
+##var current_level_index := 0  ## Only used by tutorial
+var current_loaded_level : BaseProceduralLevel = null
 
 func _ready() -> void:
 	minimap_camera.minimap_ready.connect(on_minimap_ready.bind())
-	GameState.register_level(test_procedural_level)	 ## Register the currently loaded level
 	
-	## Connect the signal for restart - ONLY USED BY TUTORIAL 
-	##GameEvents.level_restarted.connect(on_level_restarted)	## Only used by tutorial
-	##load_level(current_level_index) ## Only used by tutorial
+	## Connect the signal for restart 
+	GameEvents.level_restarted.connect(on_level_restarted)	
+	load_level(0) ## load test level
 
 func on_minimap_ready() -> void:
 	##print("received minimap readyness")
-	if test_procedural_level != null:
-		minimap_camera.set_player(test_procedural_level.get_player())
+	minimap_camera.set_player(current_loaded_level.get_player())
 
 
-## WARNING: ONLY USED BY TUTORIAL 	
-## Add a BaseLevel node to the level which will hold the level itself
-#func load_level(index: int) -> void:	
-	### If there's a level loaded, clear it out
-	#if current_loaded_level != null:
-		#current_loaded_level.queue_free()
-	#
-	### Instantiate the level
-	#if LEVELS.size() > index:
-		## Instantiate it
-		#current_loaded_level = LEVELS[index].instantiate()
-		#GameState.register_level(current_loaded_level)	 ## Register the currently loaded level
-		#add_child(current_loaded_level)
+## Add a base procedural level node to the level which will hold the level itself
+func load_level(_index: int) -> void:	
+	## If there's a level loaded, clear it out
+	if current_loaded_level != null:
+		current_loaded_level.queue_free()
+	
+	## Instantiate the level (base cprocedfural level for now)
+	current_loaded_level = LEVEL.instantiate()
+	GameState.register_level(current_loaded_level)	 ## Register the currently loaded level
+	add_child(current_loaded_level)
 
-## WARNING: ONLY USED BY TUTORIAL
 ## Function to restart the Level
-#func on_level_restarted() -> void:
-	### Just load the level again passing the current level (restarting THIS level)
-	#load_level(current_level_index)
+func on_level_restarted() -> void:
+	## Just load the level again passing the current level (restarting THIS level)
+	load_level(0)
